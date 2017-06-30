277cde2f1766b991051f81bd90d50231b7a4e1ec
Eli Selkin <ejselkin@cpp.edu>
2017-06-29 23:54:40 -0700

    debug lack of throughput

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ const auth = {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
 };
+console.log(auth);
 let transporter = nodemailer.createTransport(smtpTransport({
     service: 'SES',
     auth: auth
