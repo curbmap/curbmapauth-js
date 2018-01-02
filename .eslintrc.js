de9af687845510810601029f14ee1f4c51ab2db6
Eli Selkin <ejselkin@cpp.edu>
2018-01-01 18:49:38 -0800

    auth updates, now with email is user contents added. Rework finding user by username or email

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,15 @@
 module.exports = {
     "plugins": ["node"],
-    "extends": ["eslint:recommended", "plugin:node/recommended"],
+    "parserOptions": {
+        "ecmaVersion": 2017
+    },
+    "extends": [
+        "eslint:recommended", "plugin:node/recommended"
+    ],
     "rules": {
-        "node/exports-style": ["error", "module.exports"],
+        "node/exports-style": [
+            "error", "module.exports"
+        ],
+        "indent": ["error", 2]
     }
 };
--- routes/main.js
+++ routes/main.js
@@ -1,8 +1,8 @@
-"use strict";
 const passport = require('passport');
 require('dotenv').config({path: '../curbmap.env'});
 const postgres = require('../model/postgresModels');
 const bcrypt = require('bcrypt');
+
 const saltRounds = 12;
 const uuidv1 = require('uuid/v1');
 const winston = require('winston');
@@ -13,29 +13,78 @@ const passwordLower = /[a-z]+/g;
 const passwordNum = /[0-9]+/g;
 const regexpEmail = /^[_A-Za-z0-9-+]+(.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(.[A-Za-z0-9]+)*(.[A-Za-z]{2,})$/;
 
+function userContent(user, sessionID) {
+  if (user.username) {
+    return ({
+      success: 1,
+      username: user.username,
+      role: user.role,
+      badge: `${user.badge}`,
+      badge_updatedAt: user.badge_updatedAt,
+      score: user.score,
+      score_updatedAt: user.score_updatedAt,
+      session: sessionID,
+      email: user.user_email
+    });
+  }
+  return ({});
+}
+
 function userResources(app, transporter) {
-  app.post('/login', passport.authenticate('local'), function(req, res) {
-    req.session.role = req.user.role;
-    req.session.userid = req.user.id_user;
-    res.json(userContent(req.user, req.sessionID));
+  app.post('/login', (req, res, next) => {
+    passport.authenticate('local', (err, user, info) => {
+      try {
+        if (err) {
+          return next(err);
+        }
+        if (!user) {
+          return res
+            .status(401)
+            .json({success: info.code});
+        }
+        req.logIn(user, (error) => {
+          if (error) {
+            return next(error);
+          }
+          req.session.role = req.user.role;
+          req.session.userid = req.user.id_user;
+          res
+            .status(200)
+            .json(userContent(req.user, req.sessionID));
+        });
+      } catch (error) {
+        console.log('');
+      }
+    })(req, res, next);
   });
 
-  app.post('/logout', passport.authMiddleware(), function (req, res) {
-    let user = req.session.passport.user;
+  app.post('/logout', passport.authMiddleware(), (req, res) => {
+    const user = req.session.passport.user;
+    winston.log('info', {user, other: req.session.passport.user})
     if (req.user.username !== user) {
-      res.status(200).json({"success": false});
+      res
+        .status(200)
+        .json({success: false});
     } else {
-      res.status(200).json({"success": true});
+      res
+        .status(200)
+        .json({success: true});
       req.logout();
     }
   });
 
   app.get('/resendauth', (req, res, next) => {
-    if (req.query.hasOwnProperty('username') && req.query.username !== "") {
-      postgres.User.findOne({where: {username: req.query.username}})
+    if (req.query.hasOwnProperty('username') && req.query.username !== '') {
+      postgres
+        .User
+        .findOne({
+          where: {
+            username: req.query.username
+          }
+        })
         .then((foundUser) => {
           if (foundUser !== null && foundUser !== undefined) {
-            if (foundUser.auth_token === "") {
+            if (foundUser.auth_token === '') {
               res.json({success: -1});
               next();
             } else {
@@ -47,204 +96,224 @@ function userResources(app, transporter) {
             res.json({success: 0});
             next();
           }
-        }).catch(() => {
-        res.json({success: 0});
-        next();
-      })
+        })
+        .catch(() => {
+          res.json({success: 0});
+          next();
+        });
     }
   });
 
-  app.get('/activate', function(req, res, next) {
-    if (req.query.hasOwnProperty('username') && req.query.username !== "" && req.query.token !== "") {
-      postgres.User.findOne({where: {username: req.query.username}})
+  app.get('/activate', (req, res, next) => {
+    if (req.query.hasOwnProperty('username') && req.query.username !== '' && req.query.token !== '') {
+      postgres
+        .User
+        .findOne({
+          where: {
+            username: req.query.username
+          }
+        })
         .then((foundUser) => {
           if (foundUser !== null && foundUser !== undefined) {
             if (foundUser.auth_token === req.query.token) {
-              foundUser.auth_token = "";
+              foundUser.auth_token = '';
               foundUser.authorized = 1;
-              foundUser.save()
+              foundUser.save();
             } else {
-              throw "autherror"
+              throw 'autherror';
             }
           } else {
-            throw "nouser"
+            throw 'nouser';
           }
         })
         .then(() => {
           res.redirect('http://curbmap.com');
         })
         .catch((e) => {
-          if (e === "autherror") {
+          if (e === 'autherror') {
             winston.log('main:activate', 'wrong token', req.query.username);
-            res.render('activate', {errortext: "Please make sure you copied the token correctly"});
-          } else if (e === "nouser") {
+            res.render('activate', {errortext: 'Please make sure you copied the token correctly'});
+          } else if (e === 'nouser') {
             winston.log('main:activate', req.query.username);
-            res.render('activate', {errortext: "Please check the username again"});
+            res.render('activate', {errortext: 'Please check the username again'});
           }
           winston.log('main:activate', 'no user', req.query.username);
-        })
+        });
     } else {
-      res.render('activate', {errortext: ""});
+      res.render('activate', {errortext: ''});
       next();
     }
   });
 
   app.post('/changepassword', passport.authMiddleware(), (req, res) => {
     if (req.body.username === undefined || req.body.password === undefined || req.body.newpassword === undefined || req.body.username == 'curbmaptest') {
-      res.status(200).json({success: 0});
+      res
+        .status(200)
+        .json({success: 0});
     } else {
-      let user = req.session.passport.user; // username
+      const user = req.session.passport.user; // username
       if (req.body.username !== user) {
-        res.status(200).json({success: -1});
+        res
+          .status(200)
+          .json({success: -1});
       } else {
-        // user is correct user
-        // check passwords
+        // user is correct user check passwords
         bcrypt.compare(req.body.password, req.user.password_hash, (err, result) => {
           if (err) {
-            winston.log('info', 'changepass', {user: req.body.username, err: err});
-          } else {
-            if (result) {
-              // check password meets criteria
-              if (passwordMeetsCriteria(req.body.newpassword)) {
-                changePassword(req.user, req.body.newpassword, res);
-              } else {
-                res.status(200).json({success: -3});
-              }
+            winston.log('info', 'changepass', {
+              user: req.body.username,
+              err
+            });
+          } else if (result) {
+            // check password meets criteria
+            if (passwordMeetsCriteria(req.body.newpassword)) {
+              changePassword(req.user, req.body.newpassword, res);
             } else {
-              res.status(200).json({success: -2});
+              res
+                .status(200)
+                .json({success: -3});
             }
+          } else {
+            res
+              .status(200)
+              .json({success: -2});
           }
-        })
+        });
       }
     }
-  })
+  });
 
-  app.post('/signup', function (req, res, next) {
-    if (req.body.username !== "" && req.body.password !== "" && req.body.email !== "") {
-      postgres.User.findOne({where: {username: req.body.username}})
-        .then(
-          function(foundUser) {
-            if (foundUser === null || foundUser === undefined) {
-              return postgres.User.findOne({where: {user_email: req.body.email}})
-            } else {
-              throw "usernamefound"
-            }
-          })
-        .then(function (foundEmail) {
-          if (foundEmail === null || foundEmail === undefined){
+  app.post('/signup', (req, res, next) => {
+    if (req.body.username !== '' && req.body.password !== '' && req.body.email !== '') {
+      postgres
+        .User
+        .findOne({
+          where: {
+            username: req.body.username
+          }
+        })
+        .then((foundUser) => {
+          if (foundUser === null || foundUser === undefined) {
+            return postgres
+              .User
+              .findOne({
+                where: {
+                  user_email: req.body.email
+                }
+              });
+          }
+          throw 'usernamefound';
+        })
+        .then((foundEmail) => {
+          if (foundEmail === null || foundEmail === undefined) {
             if (passwordMeetsCriteria(req.body.password)) {
               if (emailMeetsCriteria(req.body.email)) {
                 try {
-                  let newUser = postgres.User.build({
-                    username: req.body.username,
-                    password_hash: bcrypt.hashSync(req.body.password, saltRounds),
-                    user_email: req.body.email,
-                    id_user: uuidv1(),
-                    auth_token: uuidv1()
-                  });
-                  return newUser.save()
+                  const newUser = postgres
+                    .User
+                    .build({
+                      username: req.body.username,
+                      password_hash: bcrypt.hashSync(req.body.password, saltRounds),
+                      user_email: req.body.email,
+                      id_user: uuidv1(),
+                      auth_token: uuidv1()
+                    });
+                  return newUser.save();
                 } catch (_) {
-                  throw 'couldnotcreateuser'
+                  throw 'couldnotcreateuser';
                 }
               } else {
-                throw 'emaildoesnotmeetcriteria'
+                throw 'emaildoesnotmeetcriteria';
               }
             } else {
-              throw 'passworddoesnotmeetcriteria'
+              throw 'passworddoesnotmeetcriteria';
             }
           } else {
-            throw 'emailfound'
+            throw 'emailfound';
           }
         })
-        .then(function(newUser) {
+        .then((newUser) => {
           sendAuth(transporter, req.body.username, req.body.email, newUser.auth_token);
-          res.status(200).send({success: 1});
+          res
+            .status(200)
+            .send({success: 1});
         })
         .catch((error) => {
           let value;
-          switch(error) {
-            case 'usernamefound':
-              value = -1
-              break;
-            case 'emailfound':
-              value = -2
-              break;
-            case 'passworddoesnotmeetcriteria':
-              value = -3
-              break;
-            case 'emaildoesnotmeetcriteria':
-              value = -4
-              break;
-            default:
-              value = -5
-              break;
+          switch (error) {
+          case 'usernamefound':
+            value = -1;
+            break;
+          case 'emailfound':
+            value = -2;
+            break;
+          case 'passworddoesnotmeetcriteria':
+            value = -3;
+            break;
+          case 'emaildoesnotmeetcriteria':
+            value = -4;
+            break;
+          default:
+            value = -5;
+            break;
           }
-          res.status(200).send({success: value})
-        })
+          res
+            .status(200)
+            .send({success: value});
+        });
     } else {
       // no info
-      res.status(200).send({success: 0})
+      res
+        .status(200)
+        .send({success: 0});
     }
     next();
-  })
+  });
 
-  app.get('/home', function(req, res, next) {
+  app.get('/home', (req, res, next) => {
     res.render('index');
     next();
   });
 
-  app.get('/', function (req, res, next) {
+  app.get('/', (req, res, next) => {
     res.render('index');
     next();
   });
 
-  app.get('/add', function(req, res, next) {
+  app.get('/add', (req, res, next) => {
     res.render('add');
     next();
   });
 
-  app.get('/user', passport.authMiddleware(), function(req, res, next) {
+  app.get('/user', passport.authMiddleware(), (req, res, next) => {
     res.render(userContent(req.user, req.sessionID));
     next();
   });
 
-  app.get('/token', function(req, res, next) {
+  app.get('/token', (req, res, next) => {
     res.json({});
     next();
   });
 }
 
-function userContent(user, sessionID) {
-  if (user.username) {
-    return({
-      username: user.username,
-      role: user.role,
-      badge: user.badge + "",
-      badge_updatedAt: user.badge_updatedAt,
-      score: user.score,
-      score_updatedAt: user.score_updatedAt,
-      session: sessionID
-    });
-  } else {
-    return ({});
-  }
-}
-
 function sendAuth(transporter, username, email, authToken) {
-  let mailOptions = {
+  const mailOptions = {
     from: '"curbmap team" <do-not-reply@curbmap.com>',
     to: email,
     subject: 'Thank you for signing up with curbmap.com.',
-    text: 'Copy and paste this address into your browser to authenticate: https://curbmap.com/activate?username='+username+'&token='+authToken,
-    html: '<p>Dear ' + username + ',' + '</p><p>to activate your CurbMap account, click the link below.</p><a href="https://curbmap.com/activate?username='+username+'&token='+authToken+'">Click here</a> <p> Or, paste the following authentication token into your browser when you are at the curbmap site and it requests your authentication: </p><p><b>' + authToken + '<b></p><p>Sincerely, <br> The CurbMap team</p> <img src="https://curbmap.com/img/curbmap.png">'
+    text: `Copy and paste this address into your browser to authenticate: https://curbmap.com/activate?username=${username}&token=${authToken}`,
+    html: `<p>Dear ${username},` + `</p><p>to activate your CurbMap account, click the link below.</p><a href="https://curbmap.com/activate?username=${username}&token=${authToken}">Click here</a> <p> Or, paste the following authentication token into your browser when you are at the curbmap site and it requests your authentication: </p><p><b>${authToken}<b></p><p>Sincerely, <br> The CurbMap team</p> <img src="https://curbmap.com/img/curbmap.png">`
   };
 
-  // send mail with defined transport object
+    // send mail with defined transport object
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
       return error;
     }
-    winston.log('main:sendAuth', 'sending mail to: '+email, {messageId: info.messageId, response: info.response});
+    winston.log('main:sendAuth', `sending mail to: ${email}`, {
+      messageId: info.messageId,
+      response: info.response
+    });
   });
 }
 
@@ -253,23 +322,24 @@ function emailMeetsCriteria(email) {
 }
 
 function passwordMeetsCriteria(password) {
-  return (
-    password.length >= 9 && password.length <= 40 &&
-    password.match(passwordSpecial) &&
-    password.match(passwordCapital) &&
-    password.match(passwordLower) &&
-    password.match(passwordNum)
-  );
+  return (password.length >= 9 && password.length <= 40 && password.match(passwordSpecial) && password.match(passwordCapital) && password.match(passwordLower) && password.match(passwordNum));
 }
 
 function changePassword(userObject, password, res) {
-  userObject.password_hash = bcrypt.hashSync(password, saltRounds)
-  userObject.save().then(() => {
-    res.status(200).json({success: 1});
-  }).catch((err) => {
-    winston.log('warn', 'error in saving: ', userObject.username)
-    res.status(200).json({success: -3});
-  })
+  userObject.password_hash = bcrypt.hashSync(password, saltRounds);
+  userObject
+    .save()
+    .then(() => {
+      res
+        .status(200)
+        .json({success: 1});
+    })
+    .catch((err) => {
+      winston.log('warn', 'error in saving: ', userObject.username);
+      res
+        .status(200)
+        .json({success: -3});
+    });
 }
 
 module.exports = userResources;
